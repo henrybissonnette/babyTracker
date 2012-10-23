f2735c716a12865fb1975ffae0568952cb5f7b24
Henry Bissonnette <henrydbissonnette@gmail.com>
2012-10-23 09:55:38 -0500

    let there be light

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BabyTracker::Application.config.secret_token = '69dc2102e27cb5ddbb0a4d37658b2d2d2e4e736612e6b47b957f51d1ef12b56b1db1a37a20241e535cf8fa3733879e808cb5154444456a55ecd278e46969c056'
